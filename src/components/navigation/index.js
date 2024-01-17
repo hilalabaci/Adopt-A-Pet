a081e169e3696b909385eb57d9111b2fe28ca4dc
Hilal Abaci <hilalabaci55@gmail.com>
2024-01-17 15:27:22 +0000

    added navigates

--- src/components/navigation/index.js
+++ src/components/navigation/index.js
@@ -25,7 +25,12 @@ const Navigation = () => {
       <ul className="nav-links">
         <li key={"all"}>
           {/* These links should be NavLink component and add a special active class name if its an active link */}
-          <NavLink to="/" className="nav-link">
+          <NavLink
+            to="/"
+            className={({isActive}) =>
+              `nav-link ${isActive ? "nav-link-active" : ""}`
+            }
+          >
             All Pets
           </NavLink>
         </li>
@@ -36,7 +41,9 @@ const Navigation = () => {
                 <NavLink
                   to={`/${type._links.self.href.split("/").pop()}`}
                   key={type.name}
-                  className="nav-link"
+                  className={({isActive}) =>
+                    `nav-link ${isActive ? "nav-link-active" : ""}`
+                  }
                 >
                   {type.name}s
                 </NavLink>{" "}
